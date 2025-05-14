d13b7e6d9f6547bdc35de4267715739b8b46c1ba
KaymoGithub <115141399+KaymoGithub@users.noreply.github.com>
2025-05-14 20:27:21 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'kay95 Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "kay95 Bot",
+  botOwner: 'OTL Kay95', // Your name
+  ownerNumber: '27834711405', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
